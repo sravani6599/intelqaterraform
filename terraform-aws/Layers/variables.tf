b734e79b52d891b1461f782857ff384dbda0498c
sravani6599 <92807380+sravani6599@users.noreply.github.com>
2024-12-17 14:46:34 +0530

    Update variables.tf

--- terraform-aws/Layers/variables.tf
+++ terraform-aws/Layers/variables.tf
@@ -2,7 +2,7 @@ variable "aws_access_key" {
   default= "aws_access_key"
 }
 variable "aws_secret_key" {
-defaullt = "aws_secretkey"
+default = "aws_secretkey"
  }
 variable "region" {
     default = "us-west-2"
