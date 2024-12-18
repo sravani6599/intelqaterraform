3b37dce860e7df1b869ba2d977f7a5b35e23a332
sravani6599 <92807380+sravani6599@users.noreply.github.com>
2024-12-18 12:46:06 +0530

    Update main.tf

--- terraform-aws/Layers/main.tf
+++ terraform-aws/Layers/main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
     access_key = "${var.aws_access_key_id}"
     secret_key = "${var.aws_secret_access_key}"
-    region = "${var.region}"
+    region = "${var.aws_region}"
 }
 
 #module "secrets" {
